f0dc7e606b1bfb67c64f919a5e0c5795fe8a113b
Kue Her <big_guy_559@yahoo.com>
2024-08-25 20:48:43 -0700

    week 3 attendance

--- w1/main.js
+++ w1/main.js
@@ -1,1 +1,21 @@
-console.log('Hello World!')
+console.log("Hello from inside the main.js file");
+
+//let myVar;
+
+const myVar = "kue";
+const myVarType = typeof(myVar);
+console.log("myVarType" + myVarType);
+console.log('myVarType ${myVarType}');
+
+
+
+function runNow () {
+    if(myVarType === "number") {
+        console.log('will 11 run');
+    } else {
+        console.log('will 13 run');
+    }
+}
+
+runNow();
+runNow();
