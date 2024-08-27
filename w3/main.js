57ea1faaa48f1b8f2a239d8081a867bcb18ad0ce
Kue Her <big_guy_559@yahoo.com>
2024-08-27 13:41:08 -0700

    working code

--- w3/main.js
+++ w3/main.js
@@ -27,3 +27,33 @@ let carbonFootprintPoints = 0;
 
 determineHouseHoldPts(3)
 determineHouseHoldPts(4)
+
+//Consider the size of your home. A smaller home makes a smaller impact on the environment. Take the size of your home into account as you calculate your impact
+//If you have a large house, then add 10 points to your score.
+//If you have a medium-sized house, then add 7 points.
+//If you have a small house, then add 4 points.
+//If you live in an apartment, then add 2 points.
+
+function determineHouseSizePts(homeSize){
+    console.log("Calucate Home Size");
+    if (homeSize === large) {
+        homeSizePts = homeSizePts + 10;
+    } else if (homeSize === medium) {
+        homeSizePts = homeSizePts + 7;
+    } else if (homeSize === small) {
+        homeSizePts = homeSizePts + 4;
+    } else if (homeSize === apartment) {
+        homeSizePts = homeSizePts + 2;
+    }
+        
+   console.log(`based on the size of ${homeSize} the points would be ${homeSizePts}.`);
+}
+
+let homeSizePts = 0
+let large = 10
+let medium = 7
+let small = 4
+let apartment = 2
+
+determineHouseSizePts(small)
+determineHouseSizePts(apartment)
