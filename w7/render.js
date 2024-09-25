931fadc89ca199801ba224bc80d21ed02783d836
Kue Her <big_guy_559@yahoo.com>
2024-09-25 01:13:40 -0700

    setup done

---
+++ w7/render.js
@@ -0,0 +1,43 @@
+const TBL = document.getElementById("tab-data");
+
+function renderTblHeading() {
+    const table = document.createElement("table");
+    const thead = document.createElement("thead");
+    const tr = document.createElement("tr");
+    const headingTextArr = ["Name", "HouseHold", "HouseSize", "Carbon Footprint", "Action"]
+    headingTextArr.forEach(function(text) {
+      const th = document.createElement("th");
+      th.textContent = text;
+      tr.appendChild(th);
+    });
+    thead.appendChild(tr);
+    table.appendChild(thead);
+    return table;
+  }
+  
+  function renderTbl(data){
+    const table = renderTblHeading();
+    const tbody = document.createElement("tbody");
+    const tr = document.createElement("tr");
+    const trTextArr = ["kue", 3, "large", 20];
+    trTextArr.forEach(function(text){
+      const td = document.createElement("td");
+      td.textContent = text;
+      tr.appendChild(td);
+    })
+    const td = document.createElement("td");
+    const editButton = document.createElement("button");
+    const deleteButton = document.createElement("button");
+    editButton.textContent = "Edit";
+    deleteButton.textContent = "Delete";
+    td.appendChild(editButton);
+    td.appendChild(deleteButton);
+    tr.appendChild(td);
+    tbody.appendChild(tr);
+    table.appendChild(tbody);
+    TBL.appendChild(table);
+  }
+
+  export {renderTbl, renderTblHeading};
+
+  // we were getting an error because we havent run the code and submit any data to cfpData
